fb4969c38ca3362e844bdad184ba1f133ed6bffa
Dawaman43 <dawitworkujima@gmail.com>
2025-02-16 09:15:57 -0300

    Added Versel Analaytics

--- src/index.js
+++ src/index.js
@@ -1,17 +1,18 @@
-
 import React from 'react';
-import ReactDOM from 'react-dom/client'; 
+import ReactDOM from 'react-dom/client';
 import { BrowserRouter as Router } from 'react-router-dom';
 import './index.css';
 import App from './App';
-
+import { Analytics } from '@vercel/analytics/react';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 
 root.render(
   <React.StrictMode>
     <Router>
-      <App />
+      <Analytics>
+        <App />
+      </Analytics>
     </Router>
   </React.StrictMode>
 );
